d8a658845f0ec1656be865926fcf79cea853988a
Altonhe <al648228665@gmail.com>
2021-10-19 18:50:14 +0800

    add: react when connected to educational network

--- cmd/srun-login.go
+++ cmd/srun-login.go
@@ -1,16 +1,31 @@
 // Copyright 2021 E99p1ant. All rights reserved.
+// Copyright 2021 Alton. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"io/fs"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/play175/wifiNotifier"
 	log "unknwon.dev/clog/v2"
 
 	"github.com/vidar-team/srun-login/pkg/srun"
 )
+
+type config struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	SSID     string `json:"SSID"`
+	Host     string `json:"host"`
+}
 
 func main() {
 	defer log.Stop()
@@ -22,9 +37,54 @@ func main() {
 	host := flag.String("host", "https://login.hdu.edu.cn/", "")
 	username := flag.String("username", "", "")
 	password := flag.String("password", "", "")
+	SSID := flag.String("ssid", "i-HDU", "")
 	flag.Parse()
 
-	client := srun.NewClient(*host, *username, *password)
+	var c *config
+	if *username != "" && *password != "" {
+		c = &config{
+			Username: *username,
+			Password: *password,
+			SSID:     *SSID,
+			Host:     *host,
+		}
+		f, err := json.Marshal(c)
+		if err != nil {
+			log.Error("Failed to create config")
+		}
+		err = os.WriteFile("config.json", f, fs.ModePerm)
+		if err != nil {
+			log.Error("Failed to write config file: %v", err)
+		}
+	} else {
+		f, err := os.ReadFile("config.json")
+		err = json.Unmarshal(f, &c)
+		if err != nil {
+			log.Fatal("Empty authentication pair provided, failed to unmarshal config: %+v", err)
+		}
+	}
+
+	if wifiNotifier.GetCurrentSSID() == c.SSID {
+		login(c)
+	}
+
+	wifiNotifier.SetWifiNotifier(func(ssid string) {
+		if ssid == c.SSID {
+			log.Info("Switch WIFI to %s, attempt to connect", c.SSID)
+			time.Sleep(5 * time.Second)
+			login(c)
+		} else {
+			log.Info("Switch WIFI to %s, ignoring...", wifiNotifier.GetCurrentSSID())
+		}
+	})
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+}
+
+func login(c *config) {
+	client := srun.NewClient(c.Host, c.Username, c.Password)
 	challengeResp, err := client.GetChallenge()
 	if err != nil {
 		log.Fatal("Failed to get challenge %v", err)
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/pkg/errors v0.9.1
+	github.com/play175/wifiNotifier v0.0.0-20200325045133-1f760fe14a4d
 	github.com/robertkrimen/otto v0.0.0-20211008084715-4eacda02dd21
 	github.com/stretchr/testify v1.7.0
 	github.com/wuhan005/gadget v0.0.0-20210723152124-0fc7e8810653
